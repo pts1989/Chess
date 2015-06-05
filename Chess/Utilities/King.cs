ef69920c5e9bfae6bc2ff8c4728310bb8b67dd5e
pts1989 <pts1989@gmail.com>
2015-06-05 10:18:54 +0200

    Pieces added, validation (old way) checked

---
+++ Chess/Utilities/King.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Chess.Pages;
+
+namespace Chess.Utilities
+{
+    class King : Image
+    {
+        public int pieceId;
+        public Point current;
+        List<Point> allowed_targets; 
+        //void update AllowedTargets();
+        public bool makenMove(Point source,Point target)
+        {
+            //Point movementCoordinates = _2_P_Game.getMovementCoordinates(source, target);
+
+            if(allowed_targets.Contains(target))
+            {
+                current = target;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
